773631e730b28a21be4434c70c74ed61fa9365d6
Paul <paulfarrell12@gmail.com>
2023-02-14 21:23:15 +0000

    Add storageutils

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -14,11 +14,24 @@ export class MessageComponent implements OnChanges {
   timestamp: Date = new Date();
   fromMe: boolean = false;
   text: string = '';
+  isFavourite: boolean = false;
   
   //ngOnChanges(): void {
   ngOnChanges(changes: SimpleChanges): void {
     this.timestamp = this.timestampInput;
     this.fromMe = this.fromMeInput;
     this.text = this.textInput;
+    this.isFavourite = this.isFavourite;
+  }
+
+  public toggleFavourite(){
+    if (this.isFavourite == true){
+      this.isFavourite = false;
+      localStorage.removeItem(this.timestamp.toString());
+    }
+    else{
+      this.isFavourite = true;
+      localStorage.setItem(this.timestamp.toString(), this.text);
+    }
   }
 }
---
+++ src/app/util/storage-util.ts
@@ -0,0 +1,35 @@
+export default class StorageUtils {
+    static writeToStorage(keyName: string, dataToSave: any){
+        localStorage.setItem(keyName, JSON.stringify(dataToSave));
+    }
+
+    static readFromStorage(keyName: string): any{
+        try{
+            const data = localStorage.getItem(keyName);
+            if(data != null || data != undefined || data != "undefined"){
+                console.log("Stored data for key " +  keyName + ": " + data);
+                return JSON.parse(data!);
+            }
+            else{
+                console.log("Nothing in local storage with key " + keyName);
+                return null;
+            }
+        }
+        catch(e){
+            console.log('Error reading from storage: ', e);
+        }
+    }
+
+    static deleteFromStorage(keyName: string): void{
+        try{
+            localStorage.removeItem(keyName);
+        }
+        catch(e){
+            console.log('Error deleting from storage: ', e);
+        }
+    }
+
+    static clearAllStorage(): void {
+        localStorage.clear();
+    }
+}
