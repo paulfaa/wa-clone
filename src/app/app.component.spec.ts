00c4679bd01f2c1f214c5167e6f30191a911393b
Paul <paulfarrell12@gmail.com>
2023-02-18 16:56:39 +0000

    Update sample json. unit tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -11,7 +13,10 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+    });
   });
 
   it('should create the app', () => {
@@ -24,5 +29,35 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('wa');
+  });
+
+  describe('callClearMessages()', () => {
+    it('Calls clearMessages() in the message service if the current route is /view', () => {
+      //Arrange
+      spyOn(component, 'callClearMessages');
+      //component['router'].url = '/view';
+      let link = fixture.debugElement.nativeElement.querySelector('a');
+
+      //Act
+      link.click();
+
+      //Assert
+      tick();
+	    expect(component.callClearMessages).toHaveBeenCalled();
+      expect(component['messageService'].clearAllMessages).toHaveBeenCalled();
+    });
+    it('Calls does not call clearMessages() if the current route not /view', () => {
+      //Arrange
+      spyOn(component, 'callClearMessages');
+      let link = fixture.debugElement.nativeElement.querySelector('a');
+
+      //Act
+      link.click();
+
+      //Assert
+      tick();
+	    expect(component.callClearMessages).not.toHaveBeenCalled();
+      expect(component['messageService'].clearAllMessages).not.toHaveBeenCalled();
+    });
   });
 });
