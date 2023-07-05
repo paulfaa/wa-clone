1d546856f6b1218ed9dbdc5a868cd338d6d7946c
paulf <paulfarrell12@gmail.com>
2023-07-06 00:56:07 +0200

    fix icons + timestamp

--- src/app/chat-view/chat-view.component.ts
+++ src/app/chat-view/chat-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { filter, flatMap, map, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Message } from '../models/message';
 import { MessageService } from '../services/message.service';
 import { FavouritesService } from '../services/favourites.service';
@@ -15,20 +15,20 @@ export class ChatViewComponent implements OnInit {
   selectedYear: number | undefined;
   selectedMonth: number | undefined;
 
-  constructor(private messageService: MessageService, private favouritesService: FavouritesService, private messageParsingService: MessageParsingService) {
-    var map = messageParsingService.getDatesMap().entries().next().value;
+  constructor(private messageService: MessageService,
+    private favouritesService: FavouritesService,
+    private messageParsingService: MessageParsingService) {}
+
+  ngOnInit(): void {
+    var map = this.messageParsingService.getDatesMap().entries().next().value;
     const firstDate = new Date(map[0], map[1][0]-1);
     this._serviceSubscription = this.messageService.$getFilteredMessages(firstDate);
     this._serviceSubscription.subscribe();
   }
 
-  ngOnInit(): void {}
-
   public updateDateFilter(event: Date){
     console.log("emitted ", event);
     this._serviceSubscription = this.messageService.$getFilteredMessages(event);
-    //this._serviceSubscription.subscribe();
-    //need to kill old subscription
   }
 
   public toggleFavourite(eventMessage: Message){
